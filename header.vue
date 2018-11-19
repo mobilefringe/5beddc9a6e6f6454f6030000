156c05f0ea093b5529e71edd42d63c17a4d807cf
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-19 11:26:31 -0500

    Rolling Hills 1.0  /header.vue

--- header.vue
+++ header.vue
@@ -116,7 +116,7 @@
 						        <router-link :to="item.href" class="hvr-underline-from-center" :id="item.id">{{$t(item.name)}}</router-link>
 						        <ul v-if="item.sub_menu">
 						            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">
-						                <a v-if="sub_menu.external" :href="sub_menu.href" target="_blank">{{$t(sub_menu.name)}}</a>
+						                <a v-if="sub_menu.external" :href="sub_menu.href" target="_blank" class="hvr-underline-from-center">{{$t(sub_menu.name)}}</a>
 							            <router-link v-else :to="sub_menu.href">{{$t(sub_menu.name)}}</router-link>
 						            </li>
 								</ul>
