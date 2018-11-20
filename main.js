8e2633b5b47e62c544d286ee194bc86338089ba7
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-20 10:43:02 -0500

    Rolling Hills 1.0  /main.js

--- main.js
+++ main.js
@@ -84,7 +84,7 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
         created() {
             this.loadData().then(response => {
                 this.dataLoaded = true;
-                this.property.default_logo_url = "http://via.placeholder.com/600";
+                this.property.default_logo_url = "http://via.placeholder.com/600/000/ccc";
             });
 
         },
