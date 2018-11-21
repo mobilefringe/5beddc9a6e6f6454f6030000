cff31fece4bb3d6dd83030b86ec0514068aa0b4e
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-21 18:32:44 -0500

    Rolling Hills 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -78,9 +78,9 @@
         					<div class="feature-box col-sm-4" v-for="item in feature_items">
         						<p class="feature-title">{{item.name}}</p>
         						<img :src="item.image_url" class="feature-image" :alt="'Click here for more info on '+item.name"/>
-        						<router-link :to="feature.url" class="hvr-grow-shadow">
-            						<img class="top_image" :src='feature.image_url' :alt="'Click here for more info on ' +feature.name"/>
-            						<div class="feature_overlay text-uppercase ">{{feature.name}}</div>
+        						<router-link :to="item.url" class="hvr-grow-shadow">
+            						<img class="top_image" :src='item.image_url' :alt="'Click here for more info on ' +feature.name"/>
+            						<div class="feature_overlay text-uppercase ">{{item.name}}</div>
         					    </router-link>
         						<!--<p class="feature-html">{{item.html}}</p>-->
         						<!--<span class="feature_read_more">-->
