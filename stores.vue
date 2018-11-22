ad0c1f6c0ac87382815e95b11aa8fb120095ea28
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-22 17:34:03 -0500

    Rolling Hills 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -29,7 +29,7 @@
                             </div>
                         </div>
                         <div class="col-md-9">
-                            <mapplic-svg ref="mapplic_ref" :height="566" :minimap= "false" :deeplinking="false" :sidebar="false" :hovertip="true" :maxscale= "5" :storelist="allStores" :floorlist="floorList" tooltiplabel="View Store Details"></mapplic-svg>
+                            <mapplic-map ref="mapplic_ref" :height="566" :minimap= "false" :deeplinking="false" :sidebar="false" :hovertip="true" :maxscale= "5" :storelist="allStores" :floorlist="floorList" tooltiplabel="View Store Details"></mapplic-map>
                         </div>
                     </div>
                 </div>
