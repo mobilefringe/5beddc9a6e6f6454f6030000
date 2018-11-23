3c66c059f0d4918f8778513aca168c826f6a4db2
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-11-23 10:54:17 -0500

    Rolling Hills 1.0  /hours.vue

--- hours.vue
+++ hours.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="hours_page_container"> <!-- for some reason if you do not put an outer container div this component template will not render -->
-        <img style= "width: 100%;" src="https://via.placeholder.com/760x550" alt="plaza" class="show_phone">  
+        <img style= "width: 100%;" src="https://via.placeholder.com/760x550" alt="" class="show_phone">  
         <div class="page_container">
             <div class="all_hours_container">
                 <div class="row">
