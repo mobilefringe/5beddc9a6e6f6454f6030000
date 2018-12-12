f684aee47b3ebea8fa42ec8330cbd1a4f7338e5d
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2018-12-12 16:10:17 -0500

    Rolling Hills 1.0  /stores.vue

--- stores.vue
+++ stores.vue
@@ -168,7 +168,7 @@
                     this.svgMapRef.addMarker(option);
                 },
                 dropPin(store) {
-                    console.log("dropPin", store)
+                    console.log("dropPin", store, this.$refs.mapplic_ref)
                     this.$refs.mapplic_ref.showLocation(store.svgmap_region);
                 }
             }
