02f4a7b462e0ece95ef03424716f73274332537a
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-01 13:16:42 -0500

    Rolling Hills 1.0  /main.js

--- main.js
+++ main.js
@@ -78,7 +78,8 @@ require(['Vue', 'vuex', 'vue2-filters', 'vue_router', 'routes', 'datastore', 'vu
         data: function() {
             return {
                 dataLoaded: false,
-                toggle_sitemap: false
+                toggle_sitemap: false,
+                menu_items : MenuItems
             }
         },
         created() {
