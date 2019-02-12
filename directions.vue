66663518aace7ef7178e925bf1d85f8a0d6a2bc2
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-02-12 18:06:44 -0500

    Rolling Hills 1.0  /directions.vue

--- directions.vue
+++ directions.vue
@@ -40,7 +40,7 @@
                         this.hoursImage.image_url = "";
                     }
                     
-                    this.currentPage = response[0].data;
+                    this.currentPage = response[1].data;
                     
                     this.dataloaded = true;
                 });
