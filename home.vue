55b72cc4ff30f806ccf9352b2849a3c8c2b023f1
caitlin@mobilefringe.com @ CodeCloud <caitlin@mobilefringe.com @ CodeCloud>
2019-02-12 11:34:41 -0500

    Rolling Hills 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -1,104 +1,104 @@
 <template>
     <div>
-        <!--<transition name="fade">-->
-        	<div class="row page_container" v-if="dataLoaded" v-cloak>
-        	    <div class="custom_popup" v-if="show_popup">
-                    <div class="popup_form_div">
-                        <h2 class="pop_up_header p_name">Join Our eClub</h2>
-                        <img class="text-left width_100 hide_desk custom_img" :src="popup.image_url" :alt="popup.name"/>
-                        <h3 class="text-right right_desc hide_desk custom_text"></h3>
-                        <div class="custom_row">
-                            <div class="col-md-6">
-                                <section class="">
-                                    <form id="form_popup" class="" action="//mobilefringe.createsend.com/t/d/s/irudui/" method="POST">	
-                                        <input type="hidden" value="Popup" name="cm-f-tdyudu" />
-                                        <input type="hidden" value="" name="cm-name" id="cm-name" v-model="formData.name" />
-                                        <label for="FNAME" class="main_label"><span class="">First Name</span>
-                                            <input id="FNAME" name="first_name" placeholder="" class="custom_field_control" required="required" type="text" value="" v-model="formData.firstname">
+   
+    	<div class="row page_container" v-if="dataLoaded" v-cloak>
+    	    <div class="custom_popup" v-if="show_popup">
+                <div class="popup_form_div">
+                    <h2 class="pop_up_header p_name">Join Our eClub</h2>
+                    <img class="text-left width_100 hide_desk custom_img" :src="popup.image_url" :alt="popup.name"/>
+                    <h3 class="text-right right_desc hide_desk custom_text"></h3>
+                    <div class="custom_row">
+                        <div class="col-md-6">
+                            <section class="">
+                                <form id="form_popup" class="" action="//mobilefringe.createsend.com/t/d/s/irudui/" method="POST">	
+                                    <input type="hidden" value="Popup" name="cm-f-tdyudu" />
+                                    <input type="hidden" value="" name="cm-name" id="cm-name" v-model="formData.name" />
+                                    <label for="FNAME" class="main_label"><span class="">First Name</span>
+                                        <input id="FNAME" name="first_name" placeholder="" class="custom_field_control" required="required" type="text" value="" v-model="formData.firstname">
+                                    </label>
+                                    <label for="LNAME" class="main_label"><span class="">Last Name</span>
+                                        <input id="LNAME" name="last_name" placeholder="" class="custom_field_control" required="required" type="text" value=""  v-model="formData.lastname">
+                                    </label>
+                                    <label for="EMAIL" class="main_label"><span class="">Email Address</span>
+                                        <input id="EMAIL" name="cm-irudui-irudui" placeholder="" class="custom_field_control" required="required" type="email" value="">	
+                                    </label>
+                                    <label for="PCODE" class="main_label"><span class="">Postal Code</span>
+            					        <input id="PCODE" name="cm-f-jtukjj" placeholder="" class="custom_field_control" required="required" type="text" value="">
+                                    </label>
+                                    <label for="FMALEMALE" class="main_label radio_main"><span class="">Female/Male</span>
+                                        <label for="FMALEMALE-1" class="radio_inside" required="required">
+                                            <input type="radio" name="cm-f-tdyuhl" id="FMALEMALE-1" checked="checked" value="Female">
+                                            <span class="">Female</span>
                                         </label>
-                                        <label for="LNAME" class="main_label"><span class="">Last Name</span>
-                                            <input id="LNAME" name="last_name" placeholder="" class="custom_field_control" required="required" type="text" value=""  v-model="formData.lastname">
+                                        <label for="FMALEMALE-2" class="radio_inside">
+                                            <input type="radio" name="cm-f-tdyuhl" id="FMALEMALE-2" value="Male">
+                                            <span class="">Male</span>
                                         </label>
-                                        <label for="EMAIL" class="main_label"><span class="">Email Address</span>
-                                            <input id="EMAIL" name="cm-irudui-irudui" placeholder="" class="custom_field_control" required="required" type="email" value="">	
-                                        </label>
-                                        <label for="PCODE" class="main_label"><span class="">Postal Code</span>
-                					        <input id="PCODE" name="cm-f-jtukjj" placeholder="" class="custom_field_control" required="required" type="text" value="">
-                                        </label>
-                                        <label for="FMALEMALE" class="main_label radio_main"><span class="">Female/Male</span>
-                                            <label for="FMALEMALE-1" class="radio_inside" required="required">
-                                                <input type="radio" name="cm-f-tdyuhl" id="FMALEMALE-1" checked="checked" value="Female">
-                                                <span class="">Female</span>
-                                            </label>
-                                            <label for="FMALEMALE-2" class="radio_inside">
-                                                <input type="radio" name="cm-f-tdyuhl" id="FMALEMALE-2" value="Male">
-                                                <span class="">Male</span>
-                                            </label>
-                                        </label>
-                                        <label class="main_label">
-                                            <input type="checkbox" id="popup_agree" required />
-                                            I agree to receive emails from {{property.name}}.
-                                        </label>
-                                        <button type="submit" class="popup_submit">Submit</button>	
-                                    </form>	
-                                </section>
-                            </div>
-                            <div class="col-md-6 hide_phone">
-                                <img class="text-left width_100 custom_img" :src="popup.image_url" />
-                                <h2 class="text-right right_header p_name">Join Our eClub today!</h2>
-                                <h3 class="text-right right_desc custom_text"></h3>
-                            </div>
-                            <div class="clearfix"></div>
+                                    </label>
+                                    <label class="main_label">
+                                        <input type="checkbox" id="popup_agree" required />
+                                        I agree to receive emails from {{property.name}}.
+                                    </label>
+                                    <button type="submit" class="popup_submit">Submit</button>	
+                                </form>	
+                            </section>
                         </div>
+                        <div class="col-md-6 hide_phone">
+                            <img class="text-left width_100 custom_img" :src="popup.image_url" />
+                            <h2 class="text-right right_header p_name">Join Our eClub today!</h2>
+                            <h3 class="text-right right_desc custom_text"></h3>
+                        </div>
+                        <div class="clearfix"></div>
                     </div>
-                    <span class="close_popup" @click="closePopup">CLOSE</span>
                 </div>
-        		<div class="banner_div">
-        			<div class="main_banner col-sm-9">
-        				<div  class="prev"></div>
-        				<slick ref="slick" :options="slickOptions">
-        					<div class="" v-for="banner in banners" v-if="banners">
-        						<img :src="banner.image_url" class="hvr-grow-shadow" :alt="banner.name">
-        					</div>
-        				</slick>
-        				<div class="next"></div>
-        			</div>
-        			<div class="top_two hidden_phone col-sm-3">
-        				<div class="banner_features"  v-for="feature in banner_features">
-        					<router-link :to="feature.url" class="hvr-grow-shadow">
-        						<img class="top_image" :src='feature.image_url' :alt="'Click here for more info on ' +feature.name"/>
-        						<div class="feature_overlay text-uppercase ">{{feature.name}}</div>
-        					</router-link>
-        				</div>
-        			</div>
-        		</div>
-        		<div>
-        			<div class="feature-div clearfix">
-        				<!--<span>-->
-        					<div class="feature-box col-sm-4" v-for="item in feature_items">
-        						<!--<p class="feature-title">{{item.name}}</p>-->
-        						<!--<img :src="item.image_url" class="feature-image" :alt="'Click here for more info on '+item.name"/>-->
-        						<router-link :to="item.url" class=" feature_link">
-            						<img class="hvr-grow-shadow" :src='item.image_url' :alt="'Click here for more info on ' +item.name"/>
-            						<div class="feature_overlay text-uppercase ">{{item.name}}</div>
-        					    </router-link>
-        						<!--<p class="feature-html">{{item.html}}</p>-->
-        						<!--<span class="feature_read_more">-->
-        						<!--    <a v-if="item.external" :href="item.url" class="mobile_readmore" target="_blank">-->
-        						<!--        <p class="feature-readmore hvr-sweep-to-right">View {{item.name}}<i class="fa fa-chevron-right pull-right" aria-hidden="true"></i></p>-->
-        						<!--    </a>-->
-        						<!--	<router-link v-else :to="item.url" class="mobile_readmore">-->
-        						<!--		<p class="feature-readmore hvr-sweep-to-right">View {{item.name}}<i class="fa fa-chevron-right pull-right" aria-hidden="true"></i></p>-->
-        						<!--	</router-link>-->
-        						<!--</span>-->
-        					</div>
-        				<!--</span>-->
-        			</div>
-        		</div>
-        	</div>
-        <!--</transition>-->
+                <span class="close_popup" @click="closePopup">CLOSE</span>
+            </div>
+    		<div class="banner_div">
+    			<div class="main_banner col-sm-9">
+    				<div  class="prev"></div>
+    				<slick ref="slick" :options="slickOptions">
+    					<div class="" v-for="banner in banners" v-if="banners">
+    						<img :src="banner.image_url" class="hvr-grow-shadow" :alt="banner.name">
+    					</div>
+    				</slick>
+    				<div class="next"></div>
+    			</div>
+    			<div class="top_two hidden_phone col-sm-3">
+    				<div class="banner_features"  v-for="feature in banner_features">
+    					<router-link :to="feature.url" class="hvr-grow-shadow">
+    						<img class="top_image" :src='feature.image_url' :alt="'Click here for more info on ' +feature.name"/>
+    						<div class="feature_overlay text-uppercase ">{{feature.name}}</div>
+    					</router-link>
+    				</div>
+    			</div>
+    		</div>
+    		<div>
+    			<div class="feature-div clearfix">
+    				<!--<span>-->
+    					<div class="feature-box col-sm-4" v-for="item in feature_items">
+    						<!--<p class="feature-title">{{item.name}}</p>-->
+    						<!--<img :src="item.image_url" class="feature-image" :alt="'Click here for more info on '+item.name"/>-->
+    						<router-link :to="item.url" class=" feature_link">
+        						<img class="hvr-grow-shadow" :src='item.image_url' :alt="'Click here for more info on ' +item.name"/>
+        						<div class="feature_overlay text-uppercase ">{{item.name}}</div>
+    					    </router-link>
+    						<!--<p class="feature-html">{{item.html}}</p>-->
+    						<!--<span class="feature_read_more">-->
+    						<!--    <a v-if="item.external" :href="item.url" class="mobile_readmore" target="_blank">-->
+    						<!--        <p class="feature-readmore hvr-sweep-to-right">View {{item.name}}<i class="fa fa-chevron-right pull-right" aria-hidden="true"></i></p>-->
+    						<!--    </a>-->
+    						<!--	<router-link v-else :to="item.url" class="mobile_readmore">-->
+    						<!--		<p class="feature-readmore hvr-sweep-to-right">View {{item.name}}<i class="fa fa-chevron-right pull-right" aria-hidden="true"></i></p>-->
+    						<!--	</router-link>-->
+    						<!--</span>-->
+    					</div>
+    				<!--</span>-->
+    			</div>
+    		</div>
+    		<div class="margin_30"></div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3315.8160866223798!2d-118.33180808482304!3d33.79124718067779!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dd4a44b52715a9%3A0xd7414911d196d0b7!2sRolling+Hills+Plaza!5e0!3m2!1sen!2sca!4v1549987085186" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+    	</div>
     </div>
-    
 </template>
 
 <script>
