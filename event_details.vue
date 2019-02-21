bacdb8c239f882590b23127842a14e01def02cac
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-02-21 14:21:05 -0500

    Rolling Hills 1.0  /event_details.vue

--- event_details.vue
+++ event_details.vue
@@ -1,16 +1,42 @@
 <template>
 	<div class="event_dets_container page_container" v-if="currentEvent">
 		<div class="row">
-			<div class="col-sm-4 event_logo_container hidden_phone">
-				<div v-if="currentEvent.eventable_type == 'Property'">
-					<img v-if="property.default_logo_url" :src="//codecloud.cdn.speedyrails.net/sites/5beddc9a6e6f6454f6030000/image/jpeg/1544725705000/EventsImage-PlaceHolder-3Thumbnail.jpg" :alt="property.name"/>
+			<div class="col-sm-4 event_logo_container ">
+    			<div class="hidden_phone">
+    				<div v-if="currentEvent.eventable_type == 'Property'">
+    					<img v-if="property.default_logo_url" :src="property.default_logo_url" :alt="property.name"/>
+    				</div>
+    				<div v-else>
+    				    <img v-lazy="currentEvent.store.image_url" :alt="currentEvent.store.name">
+    				</div>
 				</div>
-				<div v-else>
-				    <img v-lazy="currentEvent.store.image_url" :alt="urrentEvent.store.name">
-				</div>
+				<div class="event_details_container text-left">
+    				<div>
+    					<h1>{{currentEvent.name}}</h1>
+    					<p class="event_div_date">{{currentEvent.start_date | moment("MMM D", timezone)}} - {{currentEvent.end_date | moment("MMM D", timezone)}}</p>
+    				</div>
+    			</div>
 			</div>
 			<div class="col-sm-8 event_image_container text-left">
 				<img v-if="_.includes(currentEvent.image_url, 'missing') != true" :src="currentEvent.image_url" :alt="currentEvent.name"/>
+				<div class="event_desc_container">
+    				<div class="text-left event_description">
+    					<p v-html="currentEvent.rich_description"></p>
+    				</div>
+    			</div>
+    			<social-sharing :url="$root.shareURL('events',currentEvent.slug)" :title="currentEvent.title" :description="currentEvent.body" :quote="_.truncate(currentEvent.body, {'length': 99})" twitter-user="" :media="currentEvent.image_url" inline-template >
+                    <div class="blog-social-share text-left">
+                        <p style="display: inline-block;">Share: </p>
+                        <div class="social_share" style="display: inline-block;">
+                            <network network="facebook">
+                                <i class="fa fa-facebook"></i>
+                            </network>
+                            <network network="twitter">
+                                <i class="fa fa-twitter"></i>
+                            </network>
+                        </div>
+                    </div>
+                </social-sharing>
 			</div>
 		</div>
 		<div class="row" style="margin-left:0; margin-top: 20px;">
