3132f062e19dc42a24996c9a5c90f85d06074874
ardalan@mobilefringe.com @ CodeCloud <ardalan@mobilefringe.com @ CodeCloud>
2019-02-25 15:59:25 -0500

    Rolling Hills 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -82,7 +82,7 @@
     			</div>
     		</div>
     		<div class="margin_30"></div>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3315.8160866223798!2d-118.33180808482304!3d33.79124718067779!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dd4a44b52715a9%3A0xd7414911d196d0b7!2sRolling+Hills+Plaza!5e0!3m2!1sen!2sca!4v1549987085186" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+            <!--<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3315.8160866223798!2d-118.33180808482304!3d33.79124718067779!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dd4a44b52715a9%3A0xd7414911d196d0b7!2sRolling+Hills+Plaza!5e0!3m2!1sen!2sca!4v1549987085186" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>-->
     	</div>
     </div>
 </template>
