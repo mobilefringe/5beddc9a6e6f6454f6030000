4f92a5e2000bf5c40e1d07611d4f3733120ef274
sankavy@mobilefringe.com @ CodeCloud <sankavy@mobilefringe.com @ CodeCloud>
2019-03-06 15:03:59 -0500

    Rolling Hills 1.0  /home.vue

--- home.vue
+++ home.vue
@@ -59,9 +59,9 @@
     					<div class="" v-for="banner in banners" v-if="banners">
     						<!--<img :src="banner.image_url" class="hvr-grow-shadow" :alt="banner.name">-->
     						<a v-if="banner.url" :href="banner.url">
-        						    <img :src="banner.image_url" class="hvr-grow-shadow" :alt="banner.name">
-        						</a>
-        						<img v-else :src="banner.image_url" class="hvr-grow-shadow" :alt="banner.name">
+    						    <img :src="banner.image_url" class="hvr-grow-shadow" :alt="banner.name">
+    						</a>
+    						<img v-else :src="banner.image_url" class="hvr-grow-shadow" :alt="banner.name">
     					</div>
     				</slick>
     				<div class="next"></div>
