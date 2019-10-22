6372c37a11344be8fd7b7f9d101d18098f771a49
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-22 16:41:43 -0400

    Rolling Hills 1.1  /header.vue

--- header.vue
+++ header.vue
@@ -98,40 +98,40 @@
     	<div class="menu-container hidden-phone ">
     		<div class="menu-div page_container">
     			<div class="row top_nav hidden_phone">
-					<!--<nav id="primary_nav">-->
-					<!--	<ul>-->
-						
-						
-						
-					<!--	    <li class="menu_item" v-for="item in menu_items" :id="item.id">-->
-					<!--	        <router-link v-if="item.sub_menu == undefined" :to="item.href" class="hvr-underline-from-center" :id="item.id">{{$t(item.name)}}</router-link>-->
-					<!--	        <span tabindex=0 v-if="item.sub_menu != undefined" class="hvr-underline-from-center ">{{$t(item.name)}}</span>-->
-					<!--	        <ul v-if="item.sub_menu" class="subdropdown">-->
-					<!--	            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">-->
-					<!--	                <a v-if="sub_menu.external" :href="sub_menu.href" target="_blank" class="hvr-underline-from-center">{{$t(sub_menu.name)}}</a>-->
-					<!--		            <router-link v-else :to="sub_menu.href" class="hvr-underline-from-center">{{$t(sub_menu.name)}}</router-link>-->
-					<!--	            </li>-->
-					<!--			</ul>-->
-					<!--	    </li>-->
-						
-						
-						  
-					<!--	</ul>-->
-					<!--</nav>-->
-					
-					<nav id="primary_nav" class="hidden_phone">
+					<nav id="primary_nav">
 						<ul>
+						
+						
+						
 						    <li class="menu_item" v-for="item in menu_items" :id="item.id">
-						        <router-link v-if="item.sub_menu == undefined" :to="item.href">{{$t(item.name)}}</router-link>
-						        <span tabindex=0 v-else >{{$t(item.name)}}</span>
+						        <router-link v-if="item.sub_menu == undefined" :to="item.href" class="hvr-underline-from-center" :id="item.id">{{$t(item.name)}}</router-link>
+						        <span tabindex=0 v-if="item.sub_menu != undefined" class="hvr-underline-from-center ">{{$t(item.name)}}</span>
 						        <ul v-if="item.sub_menu" class="subdropdown">
 						            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">
-						                <router-link :to="sub_menu.href">{{$t(sub_menu.name)}}</router-link>
+						                <a v-if="sub_menu.external" :href="sub_menu.href" target="_blank" class="hvr-underline-from-center">{{$t(sub_menu.name)}}</a>
+							            <router-link v-else :to="sub_menu.href" class="hvr-underline-from-center">{{$t(sub_menu.name)}}</router-link>
 						            </li>
 								</ul>
 						    </li>
+						
+						
+						  
 						</ul>
 					</nav>
+					
+					<!--<nav id="primary_nav" class="hidden_phone">-->
+					<!--	<ul>-->
+					<!--	    <li class="menu_item" v-for="item in menu_items" :id="item.id">-->
+					<!--	        <router-link v-if="item.sub_menu == undefined" :to="item.href">{{$t(item.name)}}</router-link>-->
+					<!--	        <span tabindex=0 v-else >{{$t(item.name)}}</span>-->
+					<!--	        <ul v-if="item.sub_menu" class="subdropdown">-->
+					<!--	            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">-->
+					<!--	                <router-link :to="sub_menu.href">{{$t(sub_menu.name)}}</router-link>-->
+					<!--	            </li>-->
+					<!--			</ul>-->
+					<!--	    </li>-->
+					<!--	</ul>-->
+					<!--</nav>-->
 				</div>
     		</div>
     	</div>
