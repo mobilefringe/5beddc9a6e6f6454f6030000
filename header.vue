4cce85081b5be39da71459069bab6a5c46e2b84f
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-22 16:31:52 -0400

    Rolling Hills 1.1  /header.vue

--- header.vue
+++ header.vue
@@ -123,7 +123,7 @@
 						<ul>
 						    <li class="menu_item" v-for="item in menu_items" :id="item.id">
 						        <router-link v-if="item.sub_menu == undefined" :to="item.href">{{$t(item.name)}}</router-link>
-						        <span tabindex=0 v-else>{{$t(item.name)}}</span>
+						        <span tabindex=0 v-else >{{$t(item.name)}}</span>
 						        <ul v-if="item.sub_menu" class="subdropdown">
 						            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">
 						                <router-link :to="sub_menu.href">{{$t(sub_menu.name)}}</router-link>
