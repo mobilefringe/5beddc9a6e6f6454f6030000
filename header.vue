958cea3cf2d7a84d267eb60303ec73135ad87111
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-23 13:24:52 -0400

    Rolling Hills 1.1  /header.vue

--- header.vue
+++ header.vue
@@ -102,7 +102,7 @@
 						<ul>
 						    <li class="menu_item" v-for="item in menu_items" :id="item.id">
 						        <router-link v-if="item.sub_menu == undefined" :to="item.href" class="hvr-underline-from-center">{{$t(item.name)}}</router-link>
-						        <span tabindex=0 v-if="item.sub_menu != undefined" class="hvr-underline-from-center has_sub">{{$t(item.name)}}</span>
+						        <span tabindex=0 v-if="item.sub_menu != undefined" class="hvr-underline-from-center">{{$t(item.name)}}</span>
 						        <ul v-if="item.sub_menu" class="subdropdown">
 						            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">
 							            <router-link class="hvr-underline-from-center" :to="sub_menu.href">{{$t(sub_menu.name)}}</router-link>
