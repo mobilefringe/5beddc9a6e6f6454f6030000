79f0455b0a753f8adf7f679c126aa64acc2a4338
irene@mobilefringe.com @ CodeCloud <irene@mobilefringe.com @ CodeCloud>
2019-10-23 13:11:32 -0400

    Rolling Hills 1.1  /header.vue

--- header.vue
+++ header.vue
@@ -101,11 +101,11 @@
 					<nav id="primary_nav">
 						<ul>
 						    <li class="menu_item" v-for="item in menu_items" :id="item.id">
-						        <router-link v-if="item.sub_menu == undefined" :to="item.href" class="hvr-underline-from-center" :id="item.id">{{$t(item.name)}}</router-link>
+						        <router-link v-if="item.sub_menu == undefined" :to="item.href" class="hvr-underline-from-center">{{$t(item.name)}}</router-link>
 						        <span tabindex=0 v-if="item.sub_menu != undefined" class="hvr-underline-from-center ">{{$t(item.name)}}</span>
 						        <ul v-if="item.sub_menu" class="subdropdown">
 						            <li v-for="sub_menu in item.sub_menu" class="dropdown_item">
-						                <a v-if="sub_menu.external" :href="sub_menu.href" target="_blank" class="hvr-underline-from-center">{{$t(sub_menu.name)}}</a>
+						                <!--<a v-if="sub_menu.external" :href="sub_menu.href" target="_blank" class="hvr-underline-from-center">{{$t(sub_menu.name)}}</a>-->
 							            <router-link v-else :to="sub_menu.href" class="hvr-underline-from-center">{{$t(sub_menu.name)}}</router-link>
 						            </li>
 								</ul>
